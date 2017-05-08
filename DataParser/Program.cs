458c0efa05249e9f8ff4b7e4de99bb7b5bef4daa
Zhang,Mike <mzhang@vertafore.com>
2017-05-08 10:51:02 -0700

    base setup, c# class setup for json data

---
+++ DataParser/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public class Rootobject
+    {
+        public DateTime serverUtcTime { get; set; }
+        public string token { get; set; }
+        public int status { get; set; }
+        public Result result { get; set; }
+    }
+
+    public class Result
+    {
+        public int totalRows { get; set; }
+        public Datum[] data { get; set; }
+    }
+
+    public class Datum
+    {
+        public string[] ships { get; set; }
+        public string[] businessRegions { get; set; }
+        public string company { get; set; }
+        public string defaultImageUrl { get; set; }
+        public string[] countries { get; set; }
+        public string[] departureMonths { get; set; }
+        public string[] destinationRegions { get; set; }
+        public DateTime endDate { get; set; }
+        public string endLocation { get; set; }
+        public bool hasVirtuosoVoyages { get; set; }
+        public string[] locations { get; set; }
+        public string name { get; set; }
+        public DateTime startDate { get; set; }
+        public string startLocation { get; set; }
+        public string travelLength { get; set; }
+        public string id { get; set; }
+        public string title { get; set; }
+        public bool hasPromotions { get; set; }
+        public bool hasVirtuosoBenefits { get; set; }
+    }
+
+
+}
